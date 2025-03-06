81ab7ccfde9bc4dc62ac36eb2756598bdb8030da
ArthurCim <rizqycoe@gmail.com>
2025-03-06 09:12:50 +0700

    Dasar Pewarisan

---
+++ Mi5DaPew/src/mi5dapew/A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mi5dapew;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class A {
+    int x;
+    int y;
+    
+    void TampilkanNilaiXY(){
+        System.out.println("Nilai X : "+ x +", Y :"+ y);
+    }
+}
