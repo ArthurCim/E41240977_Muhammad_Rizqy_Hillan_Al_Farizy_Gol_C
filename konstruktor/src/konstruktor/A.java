5ffef8953cfb20ce7c4c5464a2724901b2fc16d5
ArthurCim <rizqycoe@gmail.com>
2025-03-06 09:12:15 +0700

    konstruktor

---
+++ konstruktor/src/konstruktor/A.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package konstruktor;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class A {
+    A(String param1) {
+        System.out.println("konstruktor kelas A dengan parameter " + param1 +"dieksekusi...");
+    }
+}
