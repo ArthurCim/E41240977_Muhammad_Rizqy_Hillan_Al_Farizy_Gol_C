2eef51eb4db0c99e56adb2719f76e7dfda861a69
ArthurCim <rizqycoe@gmail.com>
2025-03-06 09:11:49 +0700

    Abstrak

---
+++ abestreak/src/abestreak/A.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abestreak;
+
+public abstract class A {
+    abstract public void method2();
+    
+    public void method1(){
+        System.out.println("method konkrit kelas A");
+    }
+}
---
+++ abestreak/src/abestreak/B.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abestreak;
+
+
+public class B extends A {
+    public void method2() {
+        System.out.println("method abstrak yang sudah menjadi konkrit dalam klas B");
+    }
+}
