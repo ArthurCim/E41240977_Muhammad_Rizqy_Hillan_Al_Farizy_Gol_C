ae8b931ba1a642570ad1a6618db462ba70b02de9
ArthurCim <rizqycoe@gmail.com>
2025-03-06 09:15:04 +0700

    tugas minggu lima nomer 3

---
+++ tugmi5tiga/src/tugmi5tiga/Dog.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugmi5tiga;
+
+public class Dog extends Animal {
+    public void makeNoise() {
+        System.out.println("Bark");
+    }
+}
