86080699dd4da0739de956f610101d877475f716
ArthurCim <rizqycoe@gmail.com>
2025-03-06 09:13:49 +0700

    override

---
+++ overraid/src/overraid/B.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package overraid;
+
+public class B {
+    public void tampilkanKeLayar (){
+        System.out.println("method milik xcalss b dipanggil");
+    }
+}
---
+++ overraid/src/overraid/demo.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package overraid;
+
+public class demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();
+    }
+}
